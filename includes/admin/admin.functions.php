30298ffa78e8921058444a0983f20f5ecda4fbf5
Victor <victor@mowomo.com>
2020-08-06 13:21:19 +0200

    añadido checkeo de funcion

--- includes/admin/admin.functions.php
+++ includes/admin/admin.functions.php
@@ -16,40 +16,45 @@ if (!defined('ABSPATH') || !defined('MWM_RRSS_VERSION')) {
  * @since 2.0.2
  * @author pedroMCJ95
  */
-function mwm_endpoint_get_sidebar_content( $lang = '' ) {
 
-    $return = array();
-    $curl = curl_init();
-    
-    curl_setopt_array($curl, array(
-        CURLOPT_URL => "https://www.mowomo.com/wp-json/mowomo_plugins/v1/anuncios/",
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => "",
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 0,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => "GET",
-    ));
-    
-    $response = json_decode( curl_exec($curl) );
-    curl_close($curl);
+ if(!function_exists('mwm_endpoint_get_sidebar_content')){
 
-    if ( is_array( $response ) ) {
-        $return = $response;
-
-        if ( $lang ) {
-
-            $aux_return = $return;
-            $return = array();
-
-            foreach ( $aux_return as $post ) {
-                if ( mb_substr( $post->post_title, 0, 4 ) == '['.$lang.']' ) {
-                    array_push( $return, $post );
+     function mwm_endpoint_get_sidebar_content( $lang = '' ) {
+         
+         $return = array();
+         $curl = curl_init();
+         
+         curl_setopt_array($curl, array(
+             CURLOPT_URL => "https://www.mowomo.com/wp-json/mowomo_plugins/v1/anuncios/",
+             CURLOPT_RETURNTRANSFER => true,
+             CURLOPT_ENCODING => "",
+             CURLOPT_MAXREDIRS => 10,
+             CURLOPT_TIMEOUT => 0,
+             CURLOPT_FOLLOWLOCATION => true,
+             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+             CURLOPT_CUSTOMREQUEST => "GET",
+            ));
+            
+            $response = json_decode( curl_exec($curl) );
+            curl_close($curl);
+            
+            if ( is_array( $response ) ) {
+                $return = $response;
+                
+                if ( $lang ) {
+                    
+                    $aux_return = $return;
+                    $return = array();
+                    
+                    foreach ( $aux_return as $post ) {
+                        if ( mb_substr( $post->post_title, 0, 4 ) == '['.$lang.']' ) {
+                            array_push( $return, $post );
+                        }
+                    }
                 }
             }
+            
+            return $return;
         }
-    }
-
-    return $return;
 }
+        
